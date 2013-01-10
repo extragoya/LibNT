5c93f542c45252bcc74b7b483206f317d83d87c3
unknown <Adam@dil-paris.ece.ualberta.ca>
2013-01-10 14:58:53 -0700

    Fixed some issues with CMake build generation

--- src/tests/Lattice/latticeconstrtests.cpp
+++ src/tests/Lattice/latticeconstrtests.cpp
@@ -19,30 +19,14 @@ void constructor_work(){
     BOOST_CHECK_EQUAL(test.depth(),0);
     BOOST_CHECK_EQUAL(test.data_end()-test.data_begin(),0);
 
-    /*//= Lat1(5,5,10);
-    test1.load("data/test1.bin");
-    Lat2 test2= Lat2(5,5,10);
 
-    test2.load("data/test2.bin");
-    Lat3 test3=test1*test2;
-    test3.save(_result);
-    Lat3 test3check;
-    test3check.load(_result);
-    BOOST_CHECK( test3 == test3check );*/
 
 }
 
 BOOST_AUTO_TEST_CASE( DenseLatticeConstrTests )
 {
-
-
     constructor_work<float>();
     constructor_work<double>();
     constructor_work<int32_t>();
     constructor_work<int64_t>();
-
-
-
-
-
 }
